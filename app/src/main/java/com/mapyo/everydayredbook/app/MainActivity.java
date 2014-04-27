e98c33efeef623709811aee8956ae29998b585f4
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2014-04-27 09:03:30 +0900

    RedDataRowクラスを作ったりしている途中

--- app/src/main/java/com/mapyo/everydayredbook/app/MainActivity.java
+++ app/src/main/java/com/mapyo/everydayredbook/app/MainActivity.java
@@ -4,16 +4,49 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.view.View.OnClickListener;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
+    static List<Book> dataList = new ArrayList<Book>();
+
+    ListView listView;
+    Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        setListners();
     }
 
+    protected void findViews() {
+       listView = (ListView)findViewById(R.id.list_view);
+       addButton = (Button)findViewById(R.id.button);
+    }
+
+    protected void setListners() {
+       addButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                addItem();
+                break;
+        }
+    }
+
+    protected void addItem() {
+        dataList.add(
+                new Book()
+        );
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
