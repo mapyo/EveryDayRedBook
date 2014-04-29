b0c26704098b995c280d5594fb4ea7848418b12c
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2014-04-30 07:21:22 +0900

    新しい画面を

--- app/src/main/java/com/mapyo/everydayredbook/app/MainActivity.java
+++ app/src/main/java/com/mapyo/everydayredbook/app/MainActivity.java
@@ -2,23 +2,29 @@ package com.mapyo.everydayredbook.app;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity
+        implements OnClickListener {
+
     static List<RedDataRow> dataList = new ArrayList<RedDataRow>();
 
     ListView listView;
@@ -42,8 +48,27 @@ public class MainActivity extends Activity implements OnClickListener{
 
     protected void setListeners() {
        addButton.setOnClickListener(this);
+        // ここはいずれ
+        // listView.setOnItemClickListener(this) {
+        // という形にもっていく
+        listView.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent,View view,
+                                    int position, long id) {
+                // Toast.makeText(getApplicationContext(), "テスト position: " + position + "id: " + id, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "新しい画面を開きます", Toast.LENGTH_SHORT).show();
+                // 新しい画面へ遷移する
+                // インテントのインスタンス作成
+                Intent intent = new Intent(MainActivity.this, RedDataActivity.class);
+                // 次画面のアクティビティ起動
+                startActivity(intent);
+            }
+        });
+
     }
 
+    // これは使わなくなるはずなので後で消す
     @Override
     public void onClick(View v) {
         switch (v.getId()){
