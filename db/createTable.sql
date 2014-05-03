df518c2c7df90a9c2697089605d10d802e0bcdf8
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2014-05-03 15:06:54 +0900

    DBの作り方、テーブル定義、データを追加

---
+++ db/createTable.sql
@@ -0,0 +1,8 @@
+create table red_data(
+  id integer primary key autoincrement,
+  category text,
+  taxon text,
+  japanese_name text,
+  scientific_name text
+);
+
---
+++ db/readme.md
@@ -0,0 +1,9 @@
+# テーブルの作成とデータの投入の仕方
+
+```
+# テーブルの作成
+$ sqlite3 redbook.db < createTable.sql
+
+# データの投入
+$ sqlite3 redbook.db < redData.sql
+```
