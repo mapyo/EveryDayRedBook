21671b212a912e167561830c4bdf32e593c0894c
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2014-05-04 11:30:42 +0900

    テーブル定義のidを_idに変更

--- app/src/main/java/com/mapyo/everydayredbook/app/DataBaseHelper.java
+++ app/src/main/java/com/mapyo/everydayredbook/app/DataBaseHelper.java
@@ -10,7 +10,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.sql.SQLException;
 
 
 public class DataBaseHelper extends SQLiteOpenHelper {
--- db/createTable.sql
+++ db/createTable.sql
@@ -1,5 +1,5 @@
 create table red_data(
-  id integer primary key autoincrement,
+  _id integer primary key autoincrement,
   category text,
   taxon text,
   japanese_name text,
