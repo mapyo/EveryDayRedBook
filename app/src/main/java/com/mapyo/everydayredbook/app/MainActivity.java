184089f87d3a782ba31561d502d3f418a66c4748
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2014-05-15 09:18:02 +0900

    不要なimportを削除

--- app/src/main/java/com/mapyo/everydayredbook/app/MainActivity.java
+++ app/src/main/java/com/mapyo/everydayredbook/app/MainActivity.java
@@ -1,7 +1,6 @@
 package com.mapyo.everydayredbook.app;
 
 import android.app.Activity;
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
