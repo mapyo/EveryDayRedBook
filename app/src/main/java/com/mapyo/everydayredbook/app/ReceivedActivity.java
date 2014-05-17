ef2a91d910a04b4aeb8e5caa9e3f00ef5f7943e2
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2014-05-17 17:20:26 +0900

    追加するのを忘れていたファイル達を追加

---
+++ app/src/main/java/com/mapyo/everydayredbook/app/ReceivedActivity.java
@@ -0,0 +1,14 @@
+package com.mapyo.everydayredbook.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+
+public class ReceivedActivity extends BroadcastReceiver {
+    public void onReceive(Context context, Intent intent)
+    {
+        Toast.makeText(context, "called ReceivedActivity", Toast.LENGTH_SHORT).show();
+    }
+}
